352d487b5636bf86eb69e6f559d4673788e1a03c
VickyVsV <vikusiavsv@gmail.com>
2025-03-23 18:34:30 +0200

    2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,23 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const refs = {
+  inputEl: document.querySelector(".input"),
+  btnEl: document.querySelector(".button"),
+}
+
+let userSelectedDate;
+
+refs.btnEl.addEventListener('click', () => console.log("START"));
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr("#datetime-picker", options);
